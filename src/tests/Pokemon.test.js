cece287987686a8c3897a112464f0d23148a8358
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-07-01 16:14:36 -0300

    test pokedex

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
-import { Pokemon } from '../components';
+import App from '../App';
+
+renderWithRouter(<App />);
 
 describe('Testes do componente Pokemon', () => {
   it('Teste se é renderizado um card com as informações de determinado pokémon', () => {
-    renderWithRouter(<Pokemon />);
-
-    const namePokemons = screen.getByText('paragraph', { name: /pikachu/i });
-    expect(namePokemons).toBeInTheDocument();
-
-    const typePokemons = screen.getByRole('paragraph', { type: /Electric/i });
-    expect(typePokemons).toBeInTheDocument();
-
-    const weightPokemons = screen.getByRole('paragraph', /Average weight: 6.0 kg/i);
-    expect(weightPokemons).toBeInTheDocument();
-
-    const imagePokemons = screen.getAllByRole('img', { name: /pikachu/i });
-    expect(imagePokemons).toBeInTheDocument();
+    const namePokemons = screen.getByTestId('pokemon-name');
+    const typePokemons = screen.getByTestId('pokemen-type');
+    const weightPokemons = screen.getByTestId('pokemon-weight');
+    const imagePokemons = screen.getByAltText('Pikachu-sprite');
+    expect(namePokemons).toHaveTextContent('Pikachu');
+    expect(typePokemons).toHaveTextContent('Electric');
+    expect(weightPokemons).toHaveTextContent('Average weight: 6.0 kg');
+    expect(imagePokemons).toHaveAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/…ter/sprites/pokemon/other/official-artwork/25.png');
   });
 
   it(`Teste se o card do pokémon indicado na Pokédex contém um link de navegação
 para exibir detalhes deste pokémon. O link deve possuir a URL /pokemons/<id>, onde <id>
 é o id do pokémon exibido`, () => {
-
+    const linkCard = screen.getByRole('link', { name: /More details/i });
+    expect(linkCard).toHaveAttribute('href', destLink);
+    expect(linkCard).toBeInTheDocument();
   });
 
   it(`Teste se ao clicar no link de navegação do pokémon, é
 feito o redirecionamento da aplicação para a página de detalhes de pokémon`, () => {
-
+    const redirectClick = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(redirectClick);
+    const heading = screen.getByRole('heading', { name: /Summary/i, level: 2 });
+    expect(heading).toBeInTheDocument();
   });
 
   it(`Teste também se a URL exibida no navegador muda para /pokemon/<id>, onde <id> é o
